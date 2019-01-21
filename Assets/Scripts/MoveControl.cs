016a7bd6543cc8067ebb8f769b07bbf8625d46c9
tangeping <760905035@qq.com>
2019-01-21 19:45:13 +0800

    add  test MoveControl

--- Assets/Scripts/MoveControl.cs
+++ Assets/Scripts/MoveControl.cs
@@ -11,7 +11,7 @@ public class MoveControl : MonoBehaviour {
 
     private KBEngine.Grid navigate;
 
-    public float[,] cost;
+
     private void Start()
     {
         if (AstarPath.gridGraph == null)
@@ -20,29 +20,19 @@ public class MoveControl : MonoBehaviour {
         }
 
         navigate = AstarPath.gridGraph;
-
-        cost = new float[navigate.DimX, navigate.DimY];
-
-//         for (int i = 0; i < navigate.DimX; i++)
-//         {
-//             for (int j = 0; j < navigate.DimY; j++)
-//             {
-//                 cost[i, j] = navigate.GetCellCost(new Vector2Int(i, j)).AsFloat();
-//             }
-// 
-//         }
-
         Vector2Int start = navigate.getIndex(transform.position.ToFPVector());
         Vector2Int end = navigate.getIndex(dst.position.ToFPVector());
 
-        Vector2Int[]  path  = navigate.GetPath(start,end);
+        /*Vector2Int[]  path  = navigate.GetPath(start,end);*/
+
+        Vector2Int[] path = navigate.GetPath(start, end, new AStarPathfinder(navigate,2));
         Debug.Log(gameObject.name + ",start:" + start + ",end:" + end + ",path.step:" + path.Length);
 
-//         for (int i = 0; i < path.Length; i++)
-//         {
-//             Debug.Log("node:" + path[i] +",position:"+ navigate.GetCenterPoint(path[i].x,path[i].y));
-//         }
-        
+        for (int i = 0; i < path.Length; i++)
+        {
+            Debug.Log("node:" + path[i] + ",position:" + navigate.GetCenterPoint(path[i].x, path[i].y));
+        }
+
     }
 
 }
